9a204b8aea0a7771f97ae352f0e1cbc3ab6f3fb5
Vyacheslav Koryavtsev <Veselshak2007@yandex.ru>
2019-08-23 05:11:20 +0300

    Side exercise done

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,2 +1,21 @@
 def classify(number):
-    pass
+    
+    if number <= 0:
+        raise ValueError('Should not be Zero or negative')
+    
+    status = ''
+    
+    if sum(get_factors_list(number)[:-1]) > number:
+        status = 'abundant'
+    if sum(get_factors_list(number)[:-1]) == number:
+        status = 'perfect'
+    if sum(get_factors_list(number)[:-1]) < number:
+        status = 'deficient'
+    
+    return status
+
+def get_factors_list(number):
+    
+    factors = [i for i in range(1, number + 1) if number % i == 0]
+    
+    return factors
